ee6d211e5a3d0dcd17d405c9f8bb8758db65c0ec
antoniogamiz <antoniogamiz10@gmail.com>
2024-02-17 16:18:53 +0100

    Improve neotree configuration

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,17 +1,27 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-  config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
-    vim.keymap.set("n", "<C-Left>", "<C-w><Left>")
-    vim.keymap.set("n", "<C-Right>", "<C-w><Right>")
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>")
+		-- vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+		vim.keymap.set("n", "<C-Left>", "<C-w><Left>")
+		vim.keymap.set("n", "<C-Right>", "<C-w><Right>")
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+				follow_current_file = {
+					enabled = true, -- This will find and focus the file in the active buffer every time
+				},
+			},
+		})
+	end,
 }
-
-
