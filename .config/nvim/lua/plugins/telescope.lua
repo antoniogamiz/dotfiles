368f4531d2851dacea1e3afee762a8ccc2004617
antoniogamizbadger <antonio.gamiz@badgermapping.com>
2024-02-20 10:40:34 +0100

    Include hidden files in ripgrep

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,11 +5,16 @@ return {
 		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-      require('telescope').setup({
-        pickers = {
-          find_files = { hidden = true }
-        }
-      })
+			require("telescope").setup({
+				pickers = {
+					find_files = { hidden = true },
+					live_grep = {
+						additional_args = function(opts)
+							return { "--hidden" }
+						end,
+					},
+				},
+			})
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
